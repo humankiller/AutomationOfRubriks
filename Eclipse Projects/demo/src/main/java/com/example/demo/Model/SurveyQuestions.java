e4ec65c79ddcc687ece38a11930fa93846b2c717
OctavioLemus <43681226+OctavioLemus@users.noreply.github.com>
2019-11-16 21:02:07 -0700

    Adding Real Questions
    Updating Questions

--- Eclipse Projects/demo/src/main/java/com/example/demo/Model/SurveyQuestions.java
+++ Eclipse Projects/demo/src/main/java/com/example/demo/Model/SurveyQuestions.java
@@ -21,9 +21,9 @@ public class SurveyQuestions {
 	
 	// This is a method to test API exposure
 	static {
-		questions.add(new Question(++questionCount, "Question1", -1));
-		questions.add(new Question(++questionCount, "Question2", -1));
-		questions.add(new Question(++questionCount, "Question3", -1));
+		questions.add(new Question(++questionCount, "The development team organizes itself to accomplish the sprint goals", -1));
+		questions.add(new Question(++questionCount, "The team determines how to deliver small increments of potentially releasable functionality", -1));
+		questions.add(new Question(++questionCount, "The team ensures stores are written to deliver vertical slices of functionality", -1));
 		questions.add(new Question(++questionCount, "Question4", -1));
 		questions.add(new Question(++questionCount, "Question5", -1));
 	}
