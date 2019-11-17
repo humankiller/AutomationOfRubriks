6b5570da6e6faaa6194da010c59aef87c1feb0b3
Derek <djalle12@asu.edu>
2019-11-16 18:22:31 -0700

    Changed question score values to -1

--- Eclipse Projects/demo/src/main/java/com/example/demo/Model/SurveyQuestions.java
+++ Eclipse Projects/demo/src/main/java/com/example/demo/Model/SurveyQuestions.java
@@ -21,11 +21,11 @@ public class SurveyQuestions {
 	
 	// This is a method to test API exposure
 	static {
-		questions.add(new Question(++questionCount, "Question1", 1));
-		questions.add(new Question(++questionCount, "Question2", 2));
-		questions.add(new Question(++questionCount, "Question3", 3));
-		questions.add(new Question(++questionCount, "Question4", 4));
-		questions.add(new Question(++questionCount, "Question5", 5));
+		questions.add(new Question(++questionCount, "Question1", -1));
+		questions.add(new Question(++questionCount, "Question2", -1));
+		questions.add(new Question(++questionCount, "Question3", -1));
+		questions.add(new Question(++questionCount, "Question4", -1));
+		questions.add(new Question(++questionCount, "Question5", -1));
 	}
 	
 	public List<Question> findAll() {
