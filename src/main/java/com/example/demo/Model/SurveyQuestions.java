2d8c876f01384ab648ef360c45cf5980794be341
Derek <djalle12@asu.edu>
2019-11-30 19:28:19 -0700

    Deploy app

--- src/main/java/com/example/demo/Model/SurveyQuestions.java
+++ src/main/java/com/example/demo/Model/SurveyQuestions.java
@@ -21,14 +21,41 @@ public class SurveyQuestions {
 	
 	// This is a method to test API exposure
 	static {
-		questions.add(new Question(++questionCount, "Question1", 1));
-		questions.add(new Question(++questionCount, "Question2", 2));
-		questions.add(new Question(++questionCount, "Question3", 3));
-		questions.add(new Question(++questionCount, "Question4", 4));
-		questions.add(new Question(++questionCount, "Question5", 5));
+		questions.add(new Question(++questionCount, "The development team organizes itself to accomplish the sprint goals", -1));
+		questions.add(new Question(++questionCount, "The team determines how to deliver small increments of potentially releasable functionality", -1));
+		questions.add(new Question(++questionCount, "The team ensures stories are written to deliver vertical slices of functionality", -1));
+		questions.add(new Question(++questionCount, "The team right-sizes stories so they can be completed in 1-3 days", -1));
+		questions.add(new Question(++questionCount, "The team is cross-functional, with all the skills necessary to deliver product value", -1));
+		questions.add(new Question(++questionCount, "Individual team members may have specialized skills and areas of focus, but accountability belongs to team as a whole", -1));
+		questions.add(new Question(++questionCount, "The team takes accountability for meeting their sprint commitments", -1));
+		questions.add(new Question(++questionCount, "Team members raise impediments", -1));
+		questions.add(new Question(++questionCount, "The team swarms (works collaboratively) when needed to complete sprint work", -1));
+		questions.add(new Question(++questionCount, "The team collaborates", -1));
+		questions.add(new Question(++questionCount, "The team has open communications", -1));
+		questions.add(new Question(++questionCount, "The team has fun", -1));
+		questions.add(new Question(++questionCount, "The team confronts challenges impacting the team", -1));
 	}
 	
+	// test comment
 	public List<Question> findAll() {
 		return questions;
 	}
+	
+	public void saveScores(List<Integer> questionScores) {
+		for(int i = 0; i < questionScores.size(); i++) {
+			questions.get(i).setQuestionScore(questionScores.get(i));
+		}
+	}
+	
+	public int calculateScore() {
+		int finalScore = 0;
+		
+		for(int i = 0; i < questions.size(); i++) {
+			finalScore += questions.get(i).getQuestionScore();
+		}
+		
+		return finalScore;
+	}
+	
+	
 }
