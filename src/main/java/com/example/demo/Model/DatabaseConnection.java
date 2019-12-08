8f25d882837074a3031c2f78685a4b6964543182
Derek <djalle12@asu.edu>
2019-12-07 20:39:45 -0700

    Working on backend functions

--- src/main/java/com/example/demo/Model/DatabaseConnection.java
+++ src/main/java/com/example/demo/Model/DatabaseConnection.java
@@ -8,55 +8,36 @@ public class DatabaseConnection {
 	
 	public static void main(String[] args) throws SQLException {
 		
-		Connection con = null;
+		Statement statement = establishConnection();
 		
-		try {
+		/*
+		 * 1. Create ResultSet object to hold the results
+		 * 2. Call function executeQuery on Statement object and pass in SQL code:
+		 * 		a. "SELECT *" means select all
+		 * 		b. "FROM question_types" means from the table that you give it (in this case question_types)
+		 */
+	
+		ResultSet results = statement.executeQuery("SELECT * FROM question_types");
+		
+		while(results.next()) { // While there are more rows in the table...
 			
-			con = DriverManager.getConnection("jdbc:postgresql://ec2-107-22-239-155.compute-1.amazonaws.com/daknuflimm0laj", "utufnbbozfaphi", "4a7b61f6d36d53dd87d281cc3786acbe2bdcaf7470f7368b46ac370c1c5dbd95");
+			String data = results.getString(1);
+			System.out.println("Fetching data by column index for row " + results.getRow() + " : " + data);
 			
-			if(con != null) { // Error checking
-				System.out.println("Database Connected");
-			}
+			data = results.getString("name"); // call getString function w/ parameter "name" (column w/ data type string in database)
+			System.out.println("Fetching data by column name for row " + results.getRow() + " : " + data);
 			
-		} catch (SQLException e) {
-			System.out.println("Could not connect to the database.");
+			data = results.getString("description"); // call getString function w/ parameter "description" (column w/ data type string in database)
+			System.out.println("Fetching data by column name for row " + results.getRow() + " : " + data);
+			
+			Date date = results.getDate("created"); // call getDate function w/ parameter "created" (column w/ data type timezone in database)
+			System.out.println("Fetching data by column name for row " + results.getRow() + " : " + date);
+			
 		}
 		
-		try {
-			
-			Statement statement = con.createStatement(); // Create a "Statement" object to do operations on
-			
-			// Establish connection object by giving it postgreSQL database information 
-			
-			/*
-			 * 1. Create ResultSet object to hold the results
-			 * 2. Call function executeQuery on Statement object and pass in SQL code:
-			 * 		a. "SELECT *" means select all
-			 * 		b. "FROM question_types" means from the table that you give it (in this case question_types)
-			 */
-			ResultSet results = statement.executeQuery("SELECT * FROM question_types");
-			
-			while(results.next()) { // While there are more rows in the table...
-				
-				String data = results.getString(1);
-				System.out.println("Fetching data by column index for row " + results.getRow() + " : " + data);
-				
-				data = results.getString("name"); // call getString function w/ parameter "name" (column w/ data type string in database)
-				System.out.println("Fetching data by column name for row " + results.getRow() + " : " + data);
-				
-				data = results.getString("description"); // call getString function w/ parameter "description" (column w/ data type string in database)
-				System.out.println("Fetching data by column name for row " + results.getRow() + " : " + data);
-				
-				Date date = results.getDate("created"); // call getDate function w/ parameter "created" (column w/ data type timezone in database)
-				System.out.println("Fetching data by column name for row " + results.getRow() + " : " + date);
-				
-			}
-			
-			
-			
-		} catch (SQLException e) {
-			System.out.println("Could not retrieve data from the database " + e.getMessage());
-		}
+		//int[] questionIDs = {3, 4, 5};
+		
+		//List<Question> questions = getQuestions(questionIDs);
 		
 		
 	}
@@ -65,25 +46,9 @@ public class DatabaseConnection {
 		
 		List<Team> teamNames = new ArrayList<>();
 		
-		Connection con = null;
+		Statement statement = establishConnection();
 		
 		try {
-			
-			con = DriverManager.getConnection("jdbc:postgresql://ec2-107-22-239-155.compute-1.amazonaws.com/daknuflimm0laj", "utufnbbozfaphi", "4a7b61f6d36d53dd87d281cc3786acbe2bdcaf7470f7368b46ac370c1c5dbd95");
-			
-			if(con != null) { // Error checking
-				System.out.println("Database Connected");
-			}
-			
-		} catch (SQLException e) {
-			System.out.println("Could not connect to the database.");
-		}
-		
-		try {
-			
-			Statement statement = con.createStatement(); // Create a "Statement" object to do operations on
-			
-			// Establish connection object by giving it postgreSQL database information 
 			
 			/*
 			 * 1. Create ResultSet object to hold the results
@@ -111,8 +76,6 @@ public class DatabaseConnection {
 			
 			return teamNames;
 			
-			
-			
 		} catch (SQLException e) {
 			
 			System.out.println("Could not retrieve data from the database " + e.getMessage());
@@ -120,8 +83,111 @@ public class DatabaseConnection {
 			return teamNames;
 		}
 		
-	}
 		
+	}
+	
+	public SurveyQuestions buildSurveyQuestionsObject() {
+		
+		SurveyQuestions surveyQuestionsToReturn = new SurveyQuestions();
+		
+		Statement statement = establishConnection();
+		
+		try {
+			
+			/*
+			 * 1. Create ResultSet object to hold the results
+			 * 2. Call function executeQuery on Statement object and pass in SQL code:
+			 * 		a. "SELECT *" means select all
+			 * 		b. "FROM question_types" means from the table that you give it (in this case question_types)
+			 */
+			ResultSet results = statement.executeQuery("SELECT * FROM survey_questions");
+			
+			while(results.next()) { // While there are more rows in the table...
+				
+				
+				
+			}
+			
+			return surveyQuestionsToReturn;
+			
+		} catch (SQLException e) {
+			
+			System.out.println("Could not retrieve data from the database " + e.getMessage());
+			
+			return surveyQuestionsToReturn;
+			
+		}
+		
+		//ResultSet results = statement.executeQuery("")
+		
+		
+	}
+	
+	
+	// A method that sets up the connection so that we can just call a function
+	public static Statement establishConnection() {
+		
+		Connection con = null;
+		
+		try {
+			
+			con = DriverManager.getConnection("jdbc:postgresql://ec2-107-22-239-155.compute-1.amazonaws.com/daknuflimm0laj", "utufnbbozfaphi", "4a7b61f6d36d53dd87d281cc3786acbe2bdcaf7470f7368b46ac370c1c5dbd95");
+			
+			Statement statement = con.createStatement(); // Create a "Statement" object to do operations on
+			
+			if(con != null) { // Error checking
+				System.out.println("Database Connected");
+			}
+			
+			return statement;
+			
+		} catch (SQLException e) {
+			System.out.println("Could not connect to the database.");
+			
+			return null;
+		}
+		
+	}
+	
+	public static List<Question> getQuestions(int[] questionIndexes) {
+		
+		List<Question> questions = new ArrayList<>();
+		
+		Statement statement = establishConnection();
+		
+		try {
+			
+			ResultSet results = statement.executeQuery("SELECT * FROM questions");
+			
+			for(int i = 0; i < questionIndexes.length; i++) {
+				
+				System.out.println("YAY");
+				
+				if(results.getInt("id") == questionIndexes[i]) {
+					
+					String question = results.getString("question");
+					
+					System.out.println(question);
+					
+					Question questionToAdd = new Question();
+					
+					questionToAdd.setQuestion(question);
+					
+					questionToAdd.setQuestionScore(0); // HARDCODED
+					
+					questions.add(questionToAdd);
+					
+				}
+				
+			}
+			
+		} catch (SQLException e) {
+			System.out.println("ERROR");
+			
+		}
+		
+		return questions;
+	}
 		
 }
 	
