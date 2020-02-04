a23731253cdece1890592d02c1642398766191a0
Derek <djalle12@asu.edu>
2020-02-04 15:46:10 -0700

    Deleted meaningless zombie code from SurveyService class

--- src/main/java/com/example/demo/Model/SurveyService.java
+++ src/main/java/com/example/demo/Model/SurveyService.java
@@ -37,42 +37,6 @@ public class SurveyService {
 	
 	{
 	
-		/*
-		SurveyType developerSurvey = new SurveyType("Developer", "A survey for the developer");
-		survey.setTypeOfSurvey(developerSurvey);
-		survey.setName("Pre-Dojo Survey");
-		
-		QuestionType fivePointType = new QuestionType("5 point question type", "A survey with a 5 point scale");
-		
-		questions.add(new Question(fivePointType, "The development team organizes itself to accomplish the sprint goals", -1));
-		questions.add(new Question(fivePointType, "The team determines how to deliver small increments of potentially releasable functionality", -1));
-		questions.add(new Question(fivePointType, "The team ensures stories are written to deliver vertical slices of functionality", -1));
-		questions.add(new Question(fivePointType, "The team right-sizes stories so they can be completed in 1-3 days", -1));
-		questions.add(new Question(fivePointType, "The team is cross-functional, with all the skills necessary to deliver product value", -1));
-		questions.add(new Question(fivePointType, "Individual team members may have specialized skills and areas of focus, but accountability belongs to team as a whole", -1));
-		questions.add(new Question(fivePointType, "The team takes accountability for meeting their sprint commitments", -1));
-		questions.add(new Question(fivePointType, "Team members raise impediments", -1));
-		questions.add(new Question(fivePointType, "The team swarms (works collaboratively) when needed to complete sprint work", -1));
-		questions.add(new Question(fivePointType, "The team collaborates", -1));
-		questions.add(new Question(fivePointType, "The team has open communications", -1));
-		questions.add(new Question(fivePointType, "The team has fun", -1));
-		questions.add(new Question(fivePointType, "The team confronts challenges impacting the team", -1));
-		
-		surveyQuestionsToReturn.setSurvey(survey);
-		surveyQuestionsToReturn.setQuestions(questions);
-		surveyQuestionsToReturn.setQuestionNumber(questions.size());
-		surveyQuestionsToReturn.setTotalScore(0);
-		
-		
-		List<SurveyQuestions> surveysToPopulateTeam = new ArrayList<>();
-		surveysToPopulateTeam.add(surveyQuestionsToReturn);
-		team1.setSurveys(surveysToPopulateTeam);
-		team1.setTeamName("Team 1");
-		
-		*/
-		
-		
-		//surveyQuestionsToReturn = new SurveyQuestions(survey, questions, questions.size(), 0);
 	
 	}
 	
